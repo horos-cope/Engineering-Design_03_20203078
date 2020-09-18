f8d17f4e3dd04eb9fd2bc591f20ea7c6c9da2874
horos-cope <66311106+horos-cope@users.noreply.github.com>
2020-09-18 16:11:16 +0900

    project_05P12

---
+++ 05_example_2.ino
@@ -0,0 +1,34 @@
+//05P12
+#define PIN_LED 7
+unsigned int toggle;
+int i;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  Serial.begin(115200); // Initialize serial port
+  while (!Serial) {
+    ; // wait for serial port to connect.
+  }
+  Serial.println("Hello World!");
+  toggle = 1;
+  digitalWrite(PIN_LED, toggle); // turn off LED.
+}
+
+void loop() {
+  toggle = toggle_state(toggle); // toggle LED value.
+  Serial.println(toggle);
+  digitalWrite(PIN_LED, toggle); // update LED status.
+  delay(1000); // wait for 1,000 milliseconds
+  for(i=0;i<11;i++)
+  {
+    toggle = toggle_state(toggle); // toggle LED value.
+    digitalWrite(PIN_LED, toggle);
+    delay(100);
+  }
+
+  while(1) {}
+}
+
+int toggle_state(int toggle) {
+  return !toggle;
+}
